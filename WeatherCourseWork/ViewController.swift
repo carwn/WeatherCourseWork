ad0d0a77fc17de53aaa85df235cac57d365e61b5
Alexander Shelikhov <carwn@bk.ru>
2022-07-10 22:00:26 +0700

    Initial Commit

---
+++ WeatherCourseWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherCourseWork
+//
+//  Created by Александр Шелихов on 10.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
